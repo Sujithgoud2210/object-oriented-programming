7aec743b5cfc8d0c54dd25cbc1f97dfeb95b7416
sujithgoud2210 <sujith200322@gmail.com>
2026-02-02 14:44:00 +0530

    first commit

---
+++ src/com/java/oop/Product.java
@@ -0,0 +1,11 @@
+package com.java.oop;
+
+public class Product {
+    public static void main(String[] args) {
+     int id;
+     String name;
+     int max_Retail_Price;
+     float discount_Percentage;
+     
+    }
+}
