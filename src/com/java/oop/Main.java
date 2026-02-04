3127b7930123673f02227e811c151fc7282d5c67
sujithgoud2210 <sujith200322@gmail.com>
2026-02-04 13:04:22 +0530

    second commit

---
+++ src/com/java/oop/Main.java
@@ -0,0 +1,31 @@
+package com.java.oop;
+
+public class Main {
+    public static void main(String[] args) {
+        Product product1;
+        Product product2;
+        product1 = new Product(1, 40000, 10, 42000, "LG", 10f, 540f, 453f, 3453f, 3453f, 45f, 34f, 34f, 3f, 342f, 43f);
+        product1.displayProduct();
+        product2 = new Product(2, 50000, 20, 55000, "samsung", 10f, 540f, 453f, 3453f, 3453f, 45f, 34f, 34f, 3f, 342f, 43f);
+//        System.out.printf("%-5d  %-15d  %-10d  %-15d %10s%n", "id", "productPrice", "quantity", "max_Retail_Price","productName");
+//        System.out.println("----------------------------------------------------");
+//        System.out.printf("%-5d   %-5d           %-5d        %-5d%n",product1.id,product1.productPrice,product1.quantity,product1.max_Retail_Price);
+//        System.out.println("product id");
+//        System.out.printf("");
+//        System.out.println("product id = " + product1.id);
+//        System.out.println("product price = " + product1.productPrice);
+//        System.out.println("product quality = " + product1.quantity);
+//        System.out.println("discount percentage = " + product1.discountPercentage);
+//        System.out.println("final price = " + product1.finalPrice);
+//        System.out.println("product name = " + product1.name);
+//        System.out.println("max retail price = " + product1.max_Retail_Price);
+//        System.out.println("discounted price = " + product1.discountedPrice);
+//        System.out.println("discount amount = " + product1.discountAmount);
+//        System.out.println("state tax amount = " + product1.stateTaxAmount);
+//        System.out.println("central tax amount = " + product1.centralTaxAmount);
+//        System.out.println("final bill = " + product1.finalBill);
+//        System.out.println("total saved = " + product1.totalSaved);
+//        System.out.println("saved amount = " + product1.savedAmount);
+//        System.out.println("product id = " + product2.id);
+    }
+}
