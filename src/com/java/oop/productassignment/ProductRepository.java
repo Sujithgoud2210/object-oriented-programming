57db66029c4f044dcd9cf153bde67c2d2ad1d893
sujithgoud2210 <sujith200322@gmail.com>
2026-02-25 22:49:42 +0530

    repo

--- src/com/java/oop/productassignment/ProductRepository.java
+++ src/com/java/oop/productassignment/ProductRepository.java
@@ -47,4 +47,140 @@ public class ProductRepository {
     public Product delete(String id){
         return null;
     }
+    public void displayUsingForEach() {
+        for (Product product : productArrayList) {
+            System.out.println(product);
+        }
+    }
+
+    // Display using Iterator
+    public void displayUsingIterator() {
+        Iterator<Product> iterator = productArrayList.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+    }
+
+    // Update
+    public Product update(String id, Product product) {
+        for (int i = 0; i < productArrayList.size(); i++) {
+            if (productArrayList.get(i).getId().equals(id)) {
+                productArrayList.set(i, product);
+                return product;
+            }
+        }
+        return null;
+    }
+
+    // Delete
+    public Product delete(String id) {
+        Iterator<Product> iterator = productArrayList.iterator();
+        while (iterator.hasNext()) {
+            Product product = iterator.next();
+            if (product.getId().equals(id)) {
+                iterator.remove();
+                return product;
+            }
+        }
+        return null;
+    }
+
+    // Sort Ascending (Manual)
+    public void sortByPriceAscending() {
+        for (int i = 0; i < productArrayList.size() - 1; i++) {
+            for (int j = i + 1; j < productArrayList.size(); j++) {
+                if (productArrayList.get(i).getMaxRetailPrice() >
+                        productArrayList.get(j).getMaxRetailPrice()) {
+
+                    Product temp = productArrayList.get(i);
+                    productArrayList.set(i, productArrayList.get(j));
+                    productArrayList.set(j, temp);
+                }
+            }
+        }
+    }
+
+    // Sort Descending (Manual)
+    public void sortByPriceDescending() {
+        for (int i = 0; i < productArrayList.size() - 1; i++) {
+            for (int j = i + 1; j < productArrayList.size(); j++) {
+                if (productArrayList.get(i).getMaxRetailPrice() <
+                        productArrayList.get(j).getMaxRetailPrice()) {
+
+                    Product temp = productArrayList.get(i);
+                    productArrayList.set(i, productArrayList.get(j));
+                    productArrayList.set(j, temp);
+                }
+            }
+        }
+    }
+
+    // Highest Price Product
+    public Product getHighestPriceProduct() {
+        Product max = productArrayList.get(0);
+        for (Product product : productArrayList) {
+            if (product.getMaxRetailPrice() > max.getMaxRetailPrice()) {
+                max = product;
+            }
+        }
+        return max;
+    }
+
+    // Least Price Product
+    public Product getLeastPriceProduct() {
+        Product min = productArrayList.get(0);
+        for (Product product : productArrayList) {
+            if (product.getMaxRetailPrice() < min.getMaxRetailPrice()) {
+                min = product;
+            }
+        }
+        return min;
+    }
+
+    // Max Discount Product
+    public Product getMaxDiscountProduct() {
+        Product max = productArrayList.get(0);
+        for (Product product : productArrayList) {
+            if (product.getDiscountPercentage() > max.getDiscountPercentage()) {
+                max = product;
+            }
+        }
+        return max;
+    }
+
+    // Total Value
+    public double getTotalValue() {
+        double total = 0;
+        for (Product product : productArrayList) {
+            total += product.getMaxRetailPrice();
+        }
+        return total;
+    }
+
+    // Average Price
+    public double getAveragePrice() {
+        return getTotalValue() / productArrayList.size();
+    }
+
+    // Count Above Price
+    public int countProductsAbovePrice(double price) {
+        int count = 0;
+        for (Product product : productArrayList) {
+            if (product.getMaxRetailPrice() > price) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    // Discount Above
+    public ArrayList<Product> getProductsWithDiscountAbove(float discount) {
+        ArrayList<Product> result = new ArrayList<>();
+        for (Product product : productArrayList) {
+            if (product.getDiscountPercentage() > discount) {
+                result.add(product);
+            }
+        }
+        return result;
+    }
 }
